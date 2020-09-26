2372bbc22b841dcb5d1f1544573ae7302bf51d06
German <glezrdg@gmail.com>
2020-09-26 01:44:46 -0400

    Primer commit y escuelas C#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Platzi Academy", 2012);
+            escuela.Pais = "Colombia";
+            escuela.Ciudad = "Bogota";
+            escuela.TipoEscuela = TiposEscuela.Primaria;
+
+            Console.WriteLine(escuela);
+        }
+    }
+}
